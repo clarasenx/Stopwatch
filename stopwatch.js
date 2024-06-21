2e6bbdfa93ffb274f4c02f4cf568afa958224abe
clarasenx <clarahelenasena@gmail.com>
2024-06-21 12:45:38 -0400

    formatação da exibição do tempo contado

--- stopwatch.js
+++ stopwatch.js
@@ -2,8 +2,9 @@ class Stopwatch {
   elapsedTimeSeconds = 0;
   intervalID = null;
   start() {
-    intervalID = setInterval(() => {
+    this.intervalID = setInterval(() => {
       this.elapsedTimeSeconds++;
+      console.log(Stopwatch.formatTime(this.elapsedTimeSeconds));
     }, 1000);
   }
   stop() {
@@ -11,6 +12,25 @@ class Stopwatch {
   }
   reset() {
     this.elapsedTimeSeconds = 0;
+  }
+  static formatTime(timeInSeconds) {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds - hours * 3600 - minutes * 60;
+    return `${Stopwatch.zeroPadding(hours)}:${Stopwatch.zeroPadding(
+      minutes
+    )}:${Stopwatch.zeroPadding(seconds)}`;
+  }
+  static zeroPadding(originalNumber, desiredAmountDigits = 2) {
+    let stringNumber = String(originalNumber);
+    const zeroesRequired = desiredAmountDigits - stringNumber.length;
+    if (zeroesRequired <= 0) {
+      return stringNumber;
+    }
+    for (let counter = 0; counter < zeroesRequired; counter++) {
+      stringNumber = `0${stringNumber}`;
+    }
+    return stringNumber;
   }
 }
 
